f0de0b15cafae7cc5f52b1ce1a7bd5cb369ee794
Igor Bălănici <igor.balanici@gmail.com>
2020-05-05 01:49:48 +0300

    First commit

---
+++ src/main/java/com/balanici/securityfull/SecurityFullApplication.java
@@ -0,0 +1,13 @@
+package com.balanici.securityfull;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityFullApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityFullApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/balanici/securityfull/SecurityFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balanici.securityfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityFullApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
